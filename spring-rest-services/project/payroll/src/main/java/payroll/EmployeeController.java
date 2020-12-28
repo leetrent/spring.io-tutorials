482f22ab1c319559ff74fcf45cd12ecd929466dd
LeeTrent <lee.trent.1@gmail.com>
2020-12-28 09:24:19 -0600

    Makes the 'one' method HATEOAS compliant

--- spring-rest-services/project/payroll/src/main/java/payroll/EmployeeController.java
+++ spring-rest-services/project/payroll/src/main/java/payroll/EmployeeController.java
@@ -2,6 +2,8 @@ package payroll;
 
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,11 +30,27 @@ public class EmployeeController {
 		return this.repository.save(newEmpl);
 	}
 	
+//	@GetMapping("/employees/{id}")
+//	Employee one(@PathVariable Long id) {
+//		return this.repository.findById(id)
+//				              .orElseThrow( () -> new EmployeeNotFoundException(id));
+//	}
+	
 	@GetMapping("/employees/{id}")
-	Employee one(@PathVariable Long id) {
-		return this.repository.findById(id)
-				              .orElseThrow( () -> new EmployeeNotFoundException(id));
+	EntityModel<Employee> one(@PathVariable Long id) {
+		
+		Employee employee = this.repository.findById(id)
+			.orElseThrow(() -> new EmployeeNotFoundException(id));
+		
+		return EntityModel.of(employee,
+			linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+			linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+				
 	}
+	
+	
+	
+	
 	
 	@PutMapping("/employees/{id}")
 	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
